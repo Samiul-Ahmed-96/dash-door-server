783b01913566a809a77286ccd45acaab41447555
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-02-01 22:04:42 +0600

    Create Users Api

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ async function run() {
     console.log("db connected");
     const database = client.db("dash-door");
     const productsCollection = database.collection("products");
+    const usersCollection = database.collection("users");
 
     //Get All Products
     app.get("/products", async (req, res) => {
@@ -28,44 +29,49 @@ async function run() {
       const allProducts = await cursor.toArray();
       res.send(allProducts);
     });
-     //Get Single single
-     app.get("/products/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const singleProduct = await productsCollection.findOne(query);
-        res.json(singleProduct);
-      });
-       //Delete single item from Products Api
+    //Get Single single
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const singleProduct = await productsCollection.findOne(query);
+      res.json(singleProduct);
+    });
+    //Delete single item from Products Api
     app.delete("/products/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await productsCollection.deleteOne(query);
-        console.log(result);
-        res.json(result);
-      });
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      console.log(result);
+      res.json(result);
+    });
 
-      //Update Single Product
+    //Update Single Product
     app.put("/products/:id", async (req, res) => {
-        const id = req.params.id;
-        const updatedItem = req.body;
-        console.log(updatedItem);
-        const filter = { _id: ObjectId(id) };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: {
-            name: updatedItem.name,
-            price: updatedItem.price,
-            brand: updatedItem.brand,
-          },
-        };
-        const result = await productsCollection.updateOne(
-          filter,
-          updateDoc,
-          options
-        );
-        res.send(result);
-      });
-   
+      const id = req.params.id;
+      const updatedItem = req.body;
+      console.log(updatedItem);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updatedItem.name,
+          price: updatedItem.price,
+          brand: updatedItem.brand,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+    //Get All Users
+    app.get("/users", async (req, res) => {
+      const cursor = await usersCollection.find({});
+      const allUsers = await cursor.toArray();
+      res.send(allUsers);
+    });
   } finally {
     // await client.close();
   }
