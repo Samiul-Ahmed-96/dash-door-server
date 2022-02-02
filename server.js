52a43714c6d10ac85f667155f59bd5ce6278ad7b
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-02-02 20:49:38 +0600

    Create Update Status Api

--- server.js
+++ server.js
@@ -93,6 +93,25 @@ async function run() {
       const allOrders = await cursor.toArray();
       res.send(allOrders);
     });
+    //Update Order Status
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedOrder = req.body;
+      console.log(updatedOrder);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updatedOrder.status,
+        },
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
     //Get Single Order
     app.get("/orders/:id", async (req, res) => {
         const id = req.params.id;
